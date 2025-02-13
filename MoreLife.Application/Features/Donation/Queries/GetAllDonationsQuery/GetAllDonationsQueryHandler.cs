64b254e320fbe7807cbbdf114a4cfef22e50e05e
Edilson Manjate <edilsonsh.manjate@gmail.com>
2025-02-13 23:19:26 +0200

    Enable Swagger, refactor handler, add ARM template
    Swagger is now enabled regardless of the environment in `Program.cs`.
    
    In `GetAllDonationsQueryHandler.cs`, the `using MediatR;` statement is added, the nested class declaration is removed, and the constructor and field initializations are reorganized for better readability. The `Handle` method is refactored to improve structure and readability, including moving the initialization of the `response` object outside the `try` block and simplifying the logic for setting the response data and message.
    
    A new ARM template file `profile.arm.json` is added to deploy a function app on Azure. The template includes parameters for the resource group name, location, and resource name, and defines resources such as a storage account, an app service plan, and the function app itself. The function app is configured to use .NET as the runtime and is set to always be on. The storage account supports HTTPS traffic only, and the app service plan is set to the Standard S1 tier.

--- MoreLife.Application/Features/Donation/Queries/GetAllDonationsQuery/GetAllDonationsQueryHandler.cs
+++ MoreLife.Application/Features/Donation/Queries/GetAllDonationsQuery/GetAllDonationsQueryHandler.cs
@@ -7,49 +7,48 @@ using MoreLife.Application.Common.Exceptions;
 using MoreLife.Application.DTOs;
 using MoreLife.Application.Repositories;
 
-namespace MoreLife.Application.Features.Donations.Queries.GetAllDonationsQuery
+namespace MoreLife.Application.Features.Donations.Queries.GetAllDonationsQuery;
+
+public class GetAllDonationsQueryHandler : IRequestHandler<GetAllDonationsQuery, BaseResponse<IEnumerable<DonationDto>>>
 {
-    public class GetAllDonationsQueryHandler : IRequestHandler<GetAllDonationsQuery, BaseResponse<IEnumerable<DonationDto>>>
+    private readonly IDonationRepository _donationRepository;
+    private readonly IDonatorRepository _donatorRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllDonationsQueryHandler(IDonationRepository donationRepository, IMapper mapper, IDonatorRepository donatorRepository)
     {
-        private readonly IDonationRepository _donationRepository;
-        private readonly IDonatorRepository _donatorRepository;
-        private readonly IMapper _mapper;
+        _donationRepository = donationRepository;
+        _mapper = mapper;
+        _donatorRepository = donatorRepository;
+    }
 
-        public GetAllDonationsQueryHandler(IDonationRepository donationRepository, IMapper mapper, IDonatorRepository donatorRepository)
+    public async Task<BaseResponse<IEnumerable<DonationDto>>> Handle(GetAllDonationsQuery request, CancellationToken cancellationToken)
+    {
+        var response = new BaseResponse<IEnumerable<DonationDto>>();
+
+        try
         {
-            _donationRepository = donationRepository;
-            _mapper = mapper;
-            _donatorRepository = donatorRepository;
-        }
+            var donations = await _donationRepository.GetAll(cancellationToken);
+            var donationDtos = _mapper.Map<IEnumerable<DonationDto>>(donations);
 
-        public async Task<BaseResponse<IEnumerable<DonationDto>>> Handle(GetAllDonationsQuery request, CancellationToken cancellationToken)
-        {
-            var response = new BaseResponse<IEnumerable<DonationDto>>();
-
-            try
+            foreach (var donationDto in donationDtos)
             {
-                var donations = await _donationRepository.GetAll(cancellationToken);
-                var donationDtos = _mapper.Map<IEnumerable<DonationDto>>(donations);
-
-                foreach (var donationDto in donationDtos)
-                {
-                    var donator = await _donatorRepository.Get(donationDto.DonatorId, cancellationToken);
-                    donationDto.DonatorName = donator.Name;
-                }
-
-                if (donations is not null)
-                {
-                    response.Data = donationDtos;
-                    response.Success = true;
-                    response.Message = "Query succeed!";
-                }
-            }
-            catch (BadRequestException ex)
-            {
-                response.Message = ex.Message;
+                var donator = await _donatorRepository.Get(donationDto.DonatorId, cancellationToken);
+                donationDto.DonatorName = donator.Name;
             }
 
-            return response;
+            if (donations is not null)
+            {
+                response.Data = donationDtos;
+                response.Success = true;
+                response.Message = "Query succeed!";
+            }
         }
+        catch (BadRequestException ex)
+        {
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
 }
